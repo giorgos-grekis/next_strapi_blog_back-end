41bd7a0cfe0f56bd6f04505c37b09cdbf74feb00
giorgos_gr <giorgos@webdimension.gr>
2022-06-10 11:04:41 +0300

    change repo

---
+++ src/api/eventss/routes/eventss.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * eventss router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::eventss.eventss');
---
+++ src/api/eventss/services/eventss.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * eventss service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::eventss.eventss');
